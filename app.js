82bc82c4445222ed2dbf1ea159fa31f3314a8622
Colbee Thrasher <colbeethr@gmail.com>
2026-04-10 16:00:13 -0500

    created variables in JavaScript

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ console.log(
 );
 
 // Exercise 1
-
+let lastName;
+var age;
 // Exercise 2
+const firstName = "colbee";
+lastName = "thrasher";
+age = '21'
+// Exercise 3\
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSenstive = true;
 
-// Exercise 3
+let price = 19.99;
+let isOnSale = false;
+let salePersentage = 15;
+let stock = 0 
+let inStock = false;
+let selectedSize = "m";
+
+let title = "Name of the wind";
+let auther = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 456;
+let hasRead = true;
